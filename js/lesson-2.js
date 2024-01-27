6ec0c356a5161e8281ba78b92c5af8ce3a36f371
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2024-01-27 10:52:57 +0200

    comit-task-2

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -5,11 +5,44 @@
 //то показати через alert: "Вірно!"
 //в противному випадку відобразити:"Не знаєте? ECMAScript!"
 
-const answer = prompt("Яка офіційна назва JavaScript?")
+// const answer = prompt("Яка офіційна назва JavaScript?")
 // if (answer ==="ECMAScript") {
 //     alert ("Вірно!")
 // } else{
 //     alert ("Не знаєте? ECMAScript!")
 // }
 
-alert(answer ==="ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!")
+// alert(answer ==="ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!")
+
+
+//Напишіть цикл, який виводить в консоль
+//числа от max до min по зменьшенню
+//Виведіть в консоль суму усіх парних чисел
+
+// const max = 100;
+// const min = 10;
+// let total = 0;
+
+// for (let i=max; i>=min; i-=1) {
+    
+//  console.log(i);   
+//     if (i % 2 === 0) {
+//         total += i; 
+//     }
+// }
+// console.log(total);
+
+function getNumbers (min, max) {
+    let total = 0;
+
+    for (let i=max; i>=min; i-=1) {
+    console.log(i); 
+
+        if (i % 2 === 0) {
+            total += i; 
+        }
+    }
+    return total;
+}
+console.log(getNumbers(20, 200));
+
