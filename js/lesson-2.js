0af01f5fb944c7c2fa354fcb08c7e6b1dfcb7689
Dimass1515 <di.tsvigun@gmail.com>
2024-01-27 13:02:16 +0200

    Update lesson-2.js

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -185,10 +185,54 @@
 //     for (const number of totalNumbers){
          
 //         if (typeof number === "number") {
-//             sumNumber += number;   
+//             sumNumber += number;
 //             total += 1;
 //         }
 //     }
 //         return sumNumber/total;
 // }
 // console.log(caclculateAverage(1,2,3,4,5,6,7,"ololol"));
+
+
+//Напишіть функції для роботи з масивом
+//add(name) додає ім'я до кінця колекції
+//remove(name) видаляє ім'я із колекції
+//update(oldName, newName) змінює ім'я на нове
+
+const names = ["Alla", "Petro", "Max", "Olena", "Boris"];
+
+function add(name) {
+    if (names.includes(name)) {
+        return "Користувач вже існує";
+    }
+    names.push(name)
+    return names;
+}
+console.log(add("Timur"))
+console.log(add("Timur"))
+
+
+function remove(name) {
+    const index = names.indexOf(name)
+    if (index === -1) {
+        return "Користувач не знайденний";
+        
+    }
+    names.splice(index, 1)
+    return names;
+}
+console.log(remove("Max"))
+console.log(remove("Richard"))
+
+function update(oldName, newName) {
+    const index = names.indexOf(oldName);
+     if (index === -1) {
+        return "Користувач не знайденний";
+        
+    }
+    // names.splice(index, 1, newName)
+    names[index] = newName;
+    return names;
+}
+console.log(update("Alla", "Sergei"))
+console.log(update("Alla", "Nast"))
